311336160a0f6fcfff6f237ef8726133bf279a29
Yous <guryxxx@hotmail.com>
2023-11-21 14:20:24 +0100

    Agregar archivos de proyecto.

---
+++ ECommerceComida/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ECommerceComida.Global" Language="C#" %>
---
+++ ECommerceComida/User/User.Master.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ECommerceComida.User
+{
+    public partial class User : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Request.Url.AbsoluteUri.ToString().Contains("Default.aspx"))
+            {
+                form1.Attributes.Add("class", "sub_page");
+            }
+            else
+            {
+                //Cargar el controlador
+                Control slideUserControl = (Control)Page.LoadControl("SliderUserControll.ascx");
+                
+                //Añadir el control al panel
+                pnlSliderUC.Controls.Add(slideUserControl);
+            }
+        }
+    }
+}
